5b1e652c66605c2f1999e59d83140bdad843f878
sandrauchime63 <ogochukwuuchime@gmail.com>
2026-09-03 21:04:16 +0100

    next one

--- augment.py
+++ augment.py
@@ -3,33 +3,26 @@ import cv2
 import matplotlib.pyplot as plt
 
 images_dir = Path("License-Plate-Recognition-13/train/images")
-labels_dir = Path("License-Plate-Recognition-13/train/labels")
-
-def blur_score(image_path):
-    image=cv2.imread(image_path)
-    gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    laplacian=cv2.Laplacian(gray, cv2.CV_64F)
-    score=laplacian.var()
-    return score
-
 
 images=list(images_dir.iterdir())
 #print(images_dir)
-image_index=images[10]
 
-image = cv2.imread(str(image_index))
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image_index=images[:50]
+blur_scores=[]
+for img in image_index:
+    image = cv2.imread(str(img))
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    laplacian=cv2.Laplacian(gray, cv2.CV_64F)
+    score=laplacian.var()
+    blur_scores.append((img, score))
 
-score=blur_score(image_rgb)
-print(score)
+blur_scores.sort(key=lambda x: x[1])
+for image_path, score in blur_scores[31:41]:
+    image = cv2.imread(str(image_path))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-plt.imshow(image_rgb)
-plt.title(f"Laplacian variance: {score:.2f}")
-plt.axis("off")
-plt.show()
-"""
-for image in image_path:
-    run_blur=blur_score(image)
-    if run_blur<threshold:
-        pass
-"""
+    plt.imshow(image)
+    plt.title(f"Score: {score:.2f}")
+    plt.axis("off")
+    plt.show()
