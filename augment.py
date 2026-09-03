ab7b06e2b00c27886381d7aceab1dc20d9d2bca1
sandrauchime63 <ogochukwuuchime@gmail.com>
2026-09-03 11:48:20 +0100

    another one

--- augment.py
+++ augment.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import cv2
+import matplotlib.pyplot as plt
 
 images_dir = Path("License-Plate-Recognition-13/train/images")
 labels_dir = Path("License-Plate-Recognition-13/train/labels")
@@ -10,3 +11,25 @@ def blur_score(image_path):
     laplacian=cv2.Laplacian(gray, cv2.CV_64F)
     score=laplacian.var()
     return score
+
+
+images=list(images_dir.iterdir())
+#print(images_dir)
+image_index=images[10]
+
+image = cv2.imread(str(image_index))
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+score=blur_score(image_rgb)
+print(score)
+
+plt.imshow(image_rgb)
+plt.title(f"Laplacian variance: {score:.2f}")
+plt.axis("off")
+plt.show()
+"""
+for image in image_path:
+    run_blur=blur_score(image)
+    if run_blur<threshold:
+        pass
+"""
